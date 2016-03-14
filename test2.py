f7b060852af21308842259fd29dde41f5bdb26b4
root <dyanez95@gmail.com>
2016-03-14 23:42:39 +0000

    test2 updated

--- test2.py
+++ test2.py
@@ -8,9 +8,12 @@ firebase=firebase.FirebaseApplication('https://4loop.firebaseIO.com',None)
 button1=grove.GroveButton(2)
 button2=grove.GroveButton(6)
 
+
 counter=40
 
-firebase.put('/Counter',"count",counter)
+id_edison=1
+
+firebase.put('/Counter'+str(id_edison),"count",counter)
 
 myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
@@ -23,13 +26,13 @@ while True:
 		counter-=1
 		myLcd.clear()
 		myLcd.write(str(counter))
-		firebase.put('/Counter',"count", counter) 
+		firebase.put('/Counter'+str(id_edison),"count", counter) 
 		print counter
 		time.sleep(0.1)
 	elif(button2.value()==1):
 		counter+=1
 		myLcd.clear()
 		myLcd.write(str(counter))
-		firebase.put('/Counter',"count",counter)
+		firebase.put('/Counter'+str(id_edison),"count",counter)
 		print counter
 		time.sleep(0.1)
