1ab28486f833b46820d94694f7882155b3dba3dd
Diego <dyanez95@gmail.com>
2016-03-14 15:33:35 -0600

    test lcd on counter

--- test2.py
+++ test2.py
@@ -1,11 +1,19 @@
 import time
 import pyupm_grove as grove
+import pyupm_i2clcd as lcd
 from firebase import firebase
+
 firebase=firebase.FirebaseApplication('https://4loop.firebaseIO.com',None)
 
 button=grove.GroveButton(2)
 
 counter=40
+
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+
+myLcd.setCursor(0,0)
+
+myLcd.write(counter)
 
 while True:
 	if(button.value()==1):
