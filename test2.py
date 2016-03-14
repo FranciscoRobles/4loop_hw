fe0db4f08e6fcf6a02b954edd461949f5e662145
Diego <dyanez95@gmail.com>
2016-03-14 17:46:45 -0600

    test in new fb

--- test2.py
+++ test2.py
@@ -3,17 +3,17 @@ import pyupm_grove as grove
 import pyupm_i2clcd as lcd
 from firebase import firebase
 
-firebase=firebase.FirebaseApplication('https://4loop.firebaseIO.com',None)
+firebase=firebase.FirebaseApplication('https://letsparkiot.firebaseIO.com',None)
 
 button1=grove.GroveButton(2)
 button2=grove.GroveButton(6)
 
 
-counter=40
+counter=20
 
-id_edison=1
+id_zone="A"
 
-firebase.put('/Counter'+str(id_edison),"count",counter)
+firebase.put('/Zona'+" "+id_zone,"Capacidad",counter)
 
 myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
@@ -23,16 +23,18 @@ myLcd.write(str(counter))
 
 while True:
 	if(button1.value()==1):
-		counter-=1
-		myLcd.clear()
-		myLcd.write(str(counter))
-		firebase.put('/Counter'+str(id_edison),"count", counter) 
-		print counter
-		time.sleep(0.1)
+		if(counter>0):
+			counter-=1
+			myLcd.clear()
+			myLcd.write(str(counter))
+			firebase.put('/Zona'+" "+id_zone,"Capacidad",counter)
+			print counter
+			time.sleep(0.1)
 	elif(button2.value()==1):
-		counter+=1
-		myLcd.clear()
-		myLcd.write(str(counter))
-		firebase.put('/Counter'+str(id_edison),"count",counter)
-		print counter
-		time.sleep(0.1)
+		if(counter<20):
+			counter+=1
+			myLcd.clear()
+			myLcd.write(str(counter))
+			firebase.put('/Zona'+" "+id_zone,"Capacidad",counter)
+			print counter
+			time.sleep(0.1)
