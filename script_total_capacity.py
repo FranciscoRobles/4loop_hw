922bfe36933c50734ef9302e540fc470fed8b467
Diego <dyanez95@gmail.com>
2016-03-17 22:03:55 -0600

    script to count total capacity

---
+++ script_total_capacity.py
@@ -0,0 +1,13 @@
+from firebase import firebase
+
+counter=0
+
+zones="ABCDEFGHIJ"
+
+firebase=firebase.FirebaseApplication('https://letsparkiot.firebaseIO.com',None)
+
+for s in zones:
+	res_json=firebase.get("/ITESM/General/"+s+"/Capacity",None)
+	for key in res_json:
+		counter+=res_json[key]
+firebase.post("/ITESM/General/TotalCapacity",counter)
